641292ccf78ebf420b9fe1fac78209621998e6d3
Talonachris <chris@nebuliton.de>
2025-07-07 00:28:02 +0200

    Update main.js & remove splash screen

--- main.js
+++ main.js
@@ -1,53 +1,29 @@
-const { app, BrowserWindow } = require('electron');
-const path = require('path');
-
-let mainWindow;
-let splash;
+const { app, BrowserWindow } = require("electron");
+const path = require("path");
 
 function createWindow() {
-  // Splash-Screen
-  splash = new BrowserWindow({
-    width: 600,
-    height: 400,
-    frame: false,
-    alwaysOnTop: true,
-    resizable: false,
-    center: true,
-    show: true,
-  });
-
-  splash.loadFile('splash.html');
-
-  // Hauptfenster
-  mainWindow = new BrowserWindow({
-    width: 1280,
-    height: 800,
-    show: false,
-    autoHideMenuBar: true,
-    icon: path.join(__dirname, 'assets', 'icon.png'),
+  const mainWindow = new BrowserWindow({
+    width: 1000,
+    height: 700,
     webPreferences: {
       nodeIntegration: false,
       contextIsolation: true,
       webviewTag: true
-    },
+  },
+    icon: path.join(__dirname, "assets", "icon.png"),
   });
-
-  mainWindow.loadFile('index.html');
-
-  setTimeout(() => {
-    splash.close();
-    mainWindow.show();
-  }, 3000);
+  mainWindow.setMenu(null);
+  mainWindow.loadFile("index.html");
 }
 
 app.whenReady().then(() => {
   createWindow();
 
-  app.on('activate', () => {
+  app.on("activate", function () {
     if (BrowserWindow.getAllWindows().length === 0) createWindow();
   });
 });
 
-app.on('window-all-closed', () => {
-  if (process.platform !== 'darwin') app.quit();
+app.on("window-all-closed", function () {
+  if (process.platform !== "darwin") app.quit();
 });
